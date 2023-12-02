2b8cb19c750d35c03cbb764d7e0842ca1cc8d277
w3villa-vishal-maurya <vishalprakash.maurya@w3villa.com>
2023-12-02 14:56:29 +0530

    add redis server

--- redis/client.js
+++ redis/client.js
@@ -1,14 +1,11 @@
 const redis = require("redis");
 const logger = require("../logger/logger");
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const REDIS_HOST = "103.217.132.19" || process.env.REDIS_HOST;
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
+// const REDIS_HOST = "103.217.132.19" || process.env.REDIS_HOST;
+const REDIS_URL = process.env.REDIS_URL || "redis://103.217.132.19:6379";
 
-const client = redis.createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  // Add other configuration options as needed
-})
+redis.createClient(REDIS_URL);
 
 client.connect();
 
